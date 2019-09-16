5024454bd7ecd147324d30a6d41c8573e1ccf145
NULL2048 <973071263@qq.com>
2019-09-16 14:59:18 +0800

    完成

--- 整数因子分解/src/Factorization.java
+++ 整数因子分解/src/Factorization.java
@@ -3,27 +3,35 @@ import java.util.Scanner;
 public class Factorization {
     public static int[] a = new int[50];
     public static int index = 0;
+    public static int num;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("请输入要分解的整数：");
+        System.out.print("请输入要分解的整数：");
         int n = scan.nextInt();
+        num = n;
         resolve(n);
     }
 
     public static void resolve(int n) {
         for (int i = 2; i <= n; i++) {
             if (i == n) {
-                for (int t = 0; t <= index; t++) {
-                    System.out.print(t + " ");
+                a[index++] = i;
+                System.out.print(num + " = ");
+                for (int t = 0; t < index; t++) {
+                    System.out.print(a[t] + " ");
+                    if (t != index - 1) {
+                        System.out.print("* ");
+                    }
                 }
                 System.out.println();
                 index--;
                 return;
             }
             if (n % i == 0) {
-                a[index++] = n / i;
+                a[index++] = i;
                 resolve(n / i);
+                index--;
             }
         }
     }
