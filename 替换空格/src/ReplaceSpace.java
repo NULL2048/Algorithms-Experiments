c7cf3872dca9c6ebd51bb1e77cc1ee1f01bb7891
NULL2048 <973071263@qq.com>
2020-03-19 23:14:12 +0800

    替换空格

---
+++ 替换空格/src/ReplaceSpace.java
@@ -0,0 +1,7 @@
+public class ReplaceSpace {
+    public static String replaceSpace(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (" ".equals(str.))
+        }
+    }
+}
